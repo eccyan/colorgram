de1bcf5ddcbacf24c887f43ce467e5657d40ddad
eccyan <g00.eccyan@gmail.com>
2011-02-27 02:56:33 +0900

    average server.

--- hue/avarage.scala
+++ hue/avarage.scala
@@ -1,33 +1,31 @@
 import java.net._
 import java.io._
-import java.awt._
-import javax.imageio._
 
 import scala.actors._
+import scala.collection._
 import scala.actors.Actor._
 
-case class Echo(socket: Socket)
+case class CSV(socket: Socket)
 
-object Service extends Actor {
+object Averager extends Actor {
     implicit def inputStreamWrapper(in: InputStream) =
 	new BufferedReader(new InputStreamReader(in))
     implicit def outputStreamWrapper(out: OutputStream) =
 	new PrintWriter(new OutputStreamWriter(out))
 
-def avarage
-    def echo(in: BufferedReader, out: PrintWriter) {
-	val url = new URL( in.readLine() )
-	val image  = ImageIO.read(url);
-	for (i <- 0 to image.getWidth; j <- 0 to image.getHeight) 
-		image.getRGB(i, j)  
+    def average(in: BufferedReader, out: PrintWriter) {
+	val line = in readLine
+	val numbers = line.split("[,\n]") filter ( number => number.matches("[0-9]*") )  map (number => Integer.parseInt(number) )
+	out.println( (0 /: numbers){ _ + _ }.asInstanceOf[Float] / numbers.length)
+	out.flush
     }
 
     def act() {
 	loop {
 	    receive {
-		case Echo(socket) =>
+		case CSV(socket) =>
 		    actor {
-			echo(socket.getInputStream(), socket.getOutputStream())
+			average(socket.getInputStream(), socket.getOutputStream())
 			socket.close
 		    }
 	    }
@@ -36,7 +34,7 @@ def avarage
 }
 
 object EchoServer {
-    Service.start
+    Averager.start
 
     val serverSocket = new ServerSocket(12111)
 
@@ -44,7 +42,7 @@ object EchoServer {
 	while(true) {
 	    println("about to block")
 	    val socket = serverSocket.accept()
-	    Service ! Echo(socket)
+	    Averager ! CSV(socket)
 	    println("back from actor")
 	}
     }
