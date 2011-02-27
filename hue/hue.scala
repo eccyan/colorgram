b9c6e2b48e71eaec1170e93ce4628919ac2b1564
eccyan <g00.eccyan@gmail.com>
2011-02-27 14:13:05 +0900

    get photo avarage color.

--- hue/hue.scala
+++ hue/hue.scala
@@ -1,33 +1,43 @@
 import java.net._
 import java.io._
-import java.awt._
+import java.awt.Color
 import javax.imageio._
 
 import scala.actors._
 import scala.actors.Actor._
 
-case class Echo(socket: Socket)
+case class PhotoURL(socket: Socket)
 
-object Service extends Actor {
+object HueGetter extends Actor {
     implicit def inputStreamWrapper(in: InputStream) =
 	new BufferedReader(new InputStreamReader(in))
     implicit def outputStreamWrapper(out: OutputStream) =
 	new PrintWriter(new OutputStreamWriter(out))
 
-def avarage
-    def echo(in: BufferedReader, out: PrintWriter) {
-	val url = new URL( in.readLine() )
-	val image  = ImageIO.read(url);
-	for (i <- 0 to image.getWidth; j <- 0 to image.getHeight) 
-		image.getRGB(i, j)  
+    def hue(in: BufferedReader, out: PrintWriter) {
+	val url = new URL( in readLine() )
+	val image  = ImageIO read(url)
+	val pixcels = for (i <- 0 to image.getWidth - 1; j <- 0 to image.getHeight - 1) yield image.getRGB(i, j)
+
+	val r = pixcels map(pixcel => (pixcel >>> 0x10) & 0xff)
+	val g = pixcels map(pixcel => (pixcel >>> 0x08) & 0xff)
+	val b = pixcels map(pixcel => (pixcel >>> 0x00) & 0xff)
+
+	val count = image.getWidth * image.getHeight 
+	val avgR = (0 /: r) { _ + _ } / count
+	val avgG = (0 /: g) { _ + _ } / count
+	val avgB = (0 /: b) { _ + _ } / count
+
+	out.println( (avgR << 0x10) | (avgG << 0x08) | (avgB << 0x00) )
+	out.flush
     }
 
     def act() {
 	loop {
 	    receive {
-		case Echo(socket) =>
+		case PhotoURL(socket) =>
 		    actor {
-			echo(socket.getInputStream(), socket.getOutputStream())
+			hue(socket.getInputStream(), socket.getOutputStream())
 			socket.close
 		    }
 	    }
@@ -35,8 +45,8 @@ def avarage
     }
 }
 
-object EchoServer {
-    Service.start
+object Server {
+    HueGetter start
 
     val serverSocket = new ServerSocket(12111)
 
@@ -44,10 +54,10 @@ object EchoServer {
 	while(true) {
 	    println("about to block")
 	    val socket = serverSocket.accept()
-	    Service ! Echo(socket)
+	    HueGetter ! PhotoURL(socket)
 	    println("back from actor")
 	}
     }
 }
 
-EchoServer.start
+Server start
